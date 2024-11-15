8bef2b5843a8953509869b41245fe1a3970ca766
HuyLe3011 <lpduchuy3011@gmail.com>
2024-11-15 23:39:02 +0700

    Update Main_app.py

--- Main_app.py
+++ Main_app.py
@@ -5,7 +5,7 @@ st.set_page_config(page_title="Ứng dụng LSTM",page_icon="📊")
 st.title("Ứng dụng học sâu trong phân bổ danh mục đầu tư theo chỉ báo kĩ thuật")
 
 if st.button("Đi đến CSV", use_container_width=True):
-    st.switch_page("pages/1_input-csv.py")
+    st.switch_page("pages/1_input_csv.py")
 
 if st.button("Đi đến Date", use_container_width=True):
-    st.switch_page("pages/2_input-date.py")
+    st.switch_page("pages/2_input_date.py")
