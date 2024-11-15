ac85722475a3ac17b2bc068b8cc2e5437824b2b9
HuyLe3011 <lpduchuy3011@gmail.com>
2024-11-15 22:46:20 +0700

    Update Main_app.py

--- Main_app.py
+++ Main_app.py
@@ -6,8 +6,8 @@ st.title("Ứng dụng học sâu trong phân bổ danh mục đầu tư theo ch
 
 st.write("Chọn phương pháp tải dữ liệu bạn muốn")
 
-if st.button("Đi đến CSV", use_container_width=True):
+if st.button("Sử dụng file CSV", use_container_width=True):
     st.switch_page("pages/1_input_csv.py")
 
-if st.button("Đi đến Date", use_container_width=True):
+if st.button("Lựa chọn khoảng thời gian để tiến hành phân bổ", use_container_width=True):
     st.switch_page("pages/2_input_date.py")
