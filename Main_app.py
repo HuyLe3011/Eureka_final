99376f229aaf7aa8b8490ae5b04ac181de9a9779
HuyLe3011 <lpduchuy3011@gmail.com>
2024-11-16 00:15:26 +0700

    Update Main_app.py

--- Main_app.py
+++ Main_app.py
@@ -3,9 +3,9 @@ import streamlit as st
 st.set_page_config(page_title="Ứng dụng LSTM",page_icon="📊")
 
 st.title("App phân bổ danh mục đầu tư theo chỉ báo kĩ thuật")
-
-if st.button("Đi đến CSV", use_container_width=True,icon="📁"):
+st.write("Chọn phương thức nhập dữ liệu mà bạn muốn")
+if st.button("Nhập file .csv", use_container_width=True,icon="📁"):
     st.switch_page("pages/1_input_csv.py")
 
-if st.button("Đi đến Date", use_container_width=True,icon="🗓️"):
+if st.button("Nhập khoảng thời gian thu thập dữ liệu (chỉ hỗ trợ HOSE)", use_container_width=True,icon="🗓️"):
     st.switch_page("pages/2_input_date.py")
