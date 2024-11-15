7ff6efdb4f9de3ad6c986049fc6b2fe2508ea2aa
HuyLe3011 <lpduchuy3011@gmail.com>
2024-11-16 00:22:23 +0700

    Update Main_app.py

--- Main_app.py
+++ Main_app.py
@@ -6,6 +6,5 @@ st.title("App phân bổ danh mục đầu tư theo chỉ báo kĩ thuật")
 st.write("Chọn phương thức nhập dữ liệu mà bạn muốn")
 if st.button("Nhập file .csv", use_container_width=True,icon="📁"):
     st.switch_page("pages/1_input_csv.py")
-
 if st.button("Nhập khoảng thời gian thu thập dữ liệu (chỉ hỗ trợ HOSE)", use_container_width=True,icon="🗓️"):
     st.switch_page("pages/2_input_date.py")
