459d884e6c3fa2f71fff68f9ff421eb48fac9b25
HuyLe3011 <lpduchuy3011@gmail.com>
2024-11-16 01:26:27 +0700

    Update Main_app.py

--- Main_app.py
+++ Main_app.py
@@ -10,18 +10,3 @@ if st.button("Nhập file .csv", use_container_width=True,icon="📁"):
     st.switch_page("pages/1_input_csv.py")
 if st.button("Nhập khoảng thời gian thu thập dữ liệu (chỉ hỗ trợ HOSE)", use_container_width=True,icon="🗓️"):
     st.switch_page("pages/2_input_date.py")
-
-from streamlit_drawable_canvas import st_canvas
-
-st_canvas(
-    fill_color="rgba(255, 165, 0, 0.3)", 
-    stroke_width=stroke_width,
-    stroke_color=stroke_color,
-    background_color=bg_color,
-    background_image=Image.open(bg_image) if bg_image else None,
-    update_streamlit=realtime_update,
-    height=150,
-    drawing_mode=drawing_mode,
-    point_display_radius=point_display_radius if drawing_mode == 'point' else 0,
-    key="canvas",
-)
