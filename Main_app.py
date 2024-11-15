ee9719154e2c39a3c691e4075a29379d50cc1215
HuyLe3011 <lpduchuy3011@gmail.com>
2024-11-15 22:50:12 +0700

    Update Main_app.py

--- Main_app.py
+++ Main_app.py
@@ -2,6 +2,9 @@ import streamlit as st
 
 st.set_page_config(page_title="Ứng dụng học sâu trong phân bổ danh mục đàu tư theo chỉ báo kĩ thuật")
 
+home = st.Page("Main_app.py", title="Trang Chủ", icon=":material/home:")
+home = st.Page("1_input_csv.py", title="Trang Chủ", icon=":material/home:")
+home = st.Page("2_input_date.py", title="Trang Chủ", icon=":material/home:")
 st.title("Ứng dụng học sâu trong phân bổ danh mục đầu tư theo chỉ báo kĩ thuật")
 
 st.write("Chọn phương pháp tải dữ liệu bạn muốn")
