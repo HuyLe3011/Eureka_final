705a762faa1b3298009786cf9808c12b94ab5540
HuyLe3011 <lpduchuy3011@gmail.com>
2024-11-15 23:12:14 +0700

    Update Main_app.py

--- Main_app.py
+++ Main_app.py
@@ -6,9 +6,6 @@ home = st.Page("Main_app.py", title="Trang Chủ", icon="📊")
 csv_app = st.Page("pages/1_input_csv.py", title="Nhập file csv", icon="📁")
 date_app = st.Page("pages/2_input_date.py", title="Nhập khoảng thời gian nghiên cứu", icon="📅")
 
-pg = st.navigation([home ,csv_app, date_app])
-pg.run()
-
 st.title("📊 Ứng dụng học sâu trong phân bổ danh mục đầu tư theo chỉ báo kĩ thuật")
 
 st.write("Chọn phương pháp tải dữ liệu bạn muốn")
