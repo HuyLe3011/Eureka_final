a240d3d356d70ad0655aff00daca9233e9d61a43
HuyLe3011 <lpduchuy3011@gmail.com>
2024-11-16 09:58:17 +0700

    Update Main_app.py

--- Main_app.py
+++ Main_app.py
@@ -5,6 +5,17 @@ st.set_page_config(page_title="Ứng dụng LSTM cho danh mục đầu tư",page
 st.title(":blue[App phân bổ danh mục đầu tư theo chỉ báo kĩ thuật]")
 st.write(":red[Chọn phương thức nhập dữ liệu mà bạn muốn]")
 
+custom_css = """
+<style>
+    .stButton > button {
+        border: 2px solid #4CAF50 !important;
+        font-weight: bold !important;
+    }
+</style>
+"""
+
+# Áp dụng CSS tùy chỉnh
+st.markdown(custom_css, unsafe_allow_html=True)
 
 if st.button("Nhập file .csv", use_container_width=True,icon="📁"):
     st.switch_page("pages/1_input_csv.py")
